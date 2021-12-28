4440a4ceb2f18280fe1189a6eb468d796e5fb71d
FortunatoGiuseppe <fortunatogiuseppe1997@gmail.com>
2021-12-28 11:47:33 +0100

    Creazione CreateJoinActivity #5

---
+++ app/src/main/java/com/example/mamange/CreateJoinActivity.java
@@ -0,0 +1,13 @@
+package com.example.mamange;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateJoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createjoin_group);
+    }
+}
