cbc749fe8a5a76f83ec42dd33bfbc05d4fc3012d
DiRuvoMichele <michelediru14@gmail.com>
2021-12-28 12:47:49 +0100

    Creazione layout per login

---
+++ app/src/main/java/com/example/mamange/SignupTabFragment.java
@@ -0,0 +1,21 @@
+package com.example.mamange;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SignupTabFragment extends Fragment {
+
+    @Override
+    public View onCreate(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ViewGroup root=(ViewGroup) inflater.inflate(R.layout.signup_tab_fragment, container, attachToRoot: false);
+
+        return root;
+    }
+
+
+}
