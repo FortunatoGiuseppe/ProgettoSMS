3cbb0666b468629962a3b86483549424c7af396d
FortunatoGiuseppe <fortunatogiuseppe1997@gmail.com>
2021-12-29 09:58:21 +0100

    Connessione con Firebase stabilita

--- app/src/main/java/com/example/mamange/LoginActivity.java
+++ app/src/main/java/com/example/mamange/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
@@ -19,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toast.makeText(LoginActivity.this,"Connessione Firebase Stabilita",Toast.LENGTH_LONG).show();
 
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
@@ -35,5 +37,6 @@ public class LoginActivity extends AppCompatActivity {
         tabLayout.setTranslationY(300);
         tabLayout.setAlpha(v);
         tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+
     }
 }
