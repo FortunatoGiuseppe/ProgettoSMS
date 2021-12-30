95b4a7df63c548d3154fca58ee607f2d90e63ec0
FortunatoGiuseppe <fortunatogiuseppe1997@gmail.com>
2021-12-30 19:32:10 +0100

    Connessione Login

--- app/src/main/java/com/example/mamange/LoginTabFragment.java
+++ app/src/main/java/com/example/mamange/LoginTabFragment.java
@@ -1,5 +1,6 @@
 package com.example.mamange;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.LayoutInflater;
@@ -7,11 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
@@ -21,7 +27,7 @@ public class LoginTabFragment extends Fragment {
     TextView pass;
     TextView forgetPass;
     Button login;
-    private FirebaseAuth mAuth;
+    private FirebaseAuth fAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +37,51 @@ public class LoginTabFragment extends Fragment {
         pass = root.findViewById(R.id.pass);
         forgetPass = root.findViewById(R.id.forget_pass);
         login = root.findViewById(R.id.loginBtn);
+        fAuth = FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
         return root;
+    }
+
+    public void loginUser(){
+        String emailStringa = email.getText().toString().trim();
+        String passStringa = pass.getText().toString().trim();
+
+        if(emailStringa.isEmpty()){
+            email.setError("email is required");
+            email.requestFocus();
+            return;
+        }
+        if(passStringa.isEmpty()){
+            pass.setError("password is required");
+            pass.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailStringa).matches()){
+            email.setError("Please provide valid email!");
+            email.requestFocus();
+            return;
+        }
+        if(passStringa.length() < 6){
+            pass.setError("Enter at least six character");
+            pass.requestFocus();
+            return;
+        }
+
+        fAuth.signInWithEmailAndPassword(emailStringa,passStringa).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getActivity(),"Logged in successfully!",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getActivity(),MainActivity.class));
+                } else {
+                    Toast.makeText(getActivity(),"Failed to login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
