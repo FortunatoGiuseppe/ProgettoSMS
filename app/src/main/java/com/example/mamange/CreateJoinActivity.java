01010c8421c2fbb8e496370adc23e269c0391ed2
Salvatore de Benedictis <s.debenedictis12@studenti.uniba.it>
2021-12-30 18:35:03 +0100

    Implementazione grafica Activity Create Join Group

--- app/src/main/java/com/example/mamange/CreateJoinActivity.java
+++ app/src/main/java/com/example/mamange/CreateJoinActivity.java
@@ -1,13 +1,35 @@
 package com.example.mamange;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class CreateJoinActivity extends AppCompatActivity {
+public  class CreateJoinActivity extends Fragment {
+    TextView username;
+    Button createGroupOrder;
+    Button joinGroupOrder ;
+    //private FirebaseAuth mAuth;
+
+    public ViewGroup onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_createjoin_group, container, false);
+
+        username = root.findViewById(R.id.username);
+        createGroupOrder = root.findViewById(R.id.creaGroupOrder);
+        joinGroupOrder = root.findViewById(R.id.uniscitiGroupOrder);
+        return root;
+    }
+}
+
+
+
+/*public class CreateJoinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createjoin_group);
-    }
-}
+    }*/
+
